9d00d9925abe3092fca077c743112f2841a2b9bd
nacho-gonz <nacho.gonzalez02410@gmail.com>
2024-08-26 16:28:42 -0300

    EJ2 casi terminado

--- EJ2-NLP.py
+++ EJ2-NLP.py
@@ -20,16 +20,20 @@ for div_p in primeros_div:
             print(texto + numero)
     if cant_divs == 4:
         segundos_div = div_p.find_all('div', {'class':'col-sm-4'})
-        cant_s_divs += 1
         for div_s in segundos_div:
             if cant_s_divs == 0:
                 texto = div_s.find('div', {'class':'small-box bg-red'}).find('div',{'class':'inner'}).find('p',{'style':'font-size: 110%;'}).text
                 numero = div_s.find('div', {'class':'small-box bg-red'}).find('div',{'class':'inner'}).find('h3').find('p').text
+                print(texto + numero)
             elif cant_s_divs == 1:
-                terceros_divs = div_s.find('div').find_all('div')
+                terceros_divs = div_s.find('div', {'class':'col-sm-12'}).find_all('div')
+                for div_t in terceros_divs:
+                    texto = div_t.find('div', {'class':'small-box bg-aqua'}).find('div',{'class':'inner'}).find('p',{'style':'font-size:120%'}).text
+                    numero = div_t.find('div', {'class':'small-box bg-aqua'}).find('div',{'class':'inner'}).find('h3').find('p').text
+                    print(texto + numero)
             else:
                 texto = div_s.find('div', {'class':'small-box bg-aqua'}).find('div',{'class':'inner'}).find('p',{'style':'font-size: 120%;'}).text
                 numero = div_s.find('div', {'class':'small-box bg-aqua'}).find('div',{'class':'inner'}).find('h3').find('p').text
-            print(texto + numero)
-    
+                print(texto + numero)
+            cant_s_divs += 1
     cant_divs += 1
